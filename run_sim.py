9f8d2ed3a54c9c6f04c9bca63aa2115f7a21057b
David Hörl <hoerldavid@gmail.com>
2018-09-16 19:08:08 +0200

    different ri-delta init, smaller physical size, fix flipped position with right_illum

--- run_sim.py
+++ run_sim.py
@@ -9,6 +9,10 @@ from matplotlib import pyplot as plt
 from skimage.io import imsave
 from skimage.io import imread_collection
 
+# refractive index of medium
+RI_DEFAULT = 1.33 
+# range of ri delta of tissue (in percent of RI_DEFAULT)
+RI_DELTA_RANGE = (.05, .07)
 
 def split_str_digit(s):
     """
@@ -27,7 +31,7 @@ def split_str_digit(s):
 
 
 def sim_lightsheet(base_path, out_dir, phantom_dir='sim-phantom', in_pattern='sim(\d+).tif', right_illum=False,
-                   physical_dims=(800,800,100), ls_pos=400, lam=500, out_pattern='sim-biobeam{}.tif'):
+                   physical_dims=(400,400,50), ls_pos=200, lam=500, out_pattern='sim-biobeam{}.tif', planes_to_simulate=None):
     
     in_path = os.path.join(base_path, phantom_dir)
     files = os.listdir(in_path)
@@ -46,13 +50,15 @@ def sim_lightsheet(base_path, out_dir, phantom_dir='sim-phantom', in_pattern='si
     
     # setup signal and refractive index
     signal = img
-    dn = (np.sqrt(signal / np.max(signal))) * 0.01
+    dn = signal / np.max(signal) * RI_DEFAULT * (RI_DELTA_RANGE[1] - RI_DELTA_RANGE[0]) + RI_DEFAULT * RI_DELTA_RANGE[1]
+    # no ri change where we have no signal/tissue
+    dn[signal==0] = 0
 
     #create a microscope simulator
-    m = biobeam.SimLSM_Cylindrical(dn = dn, signal = signal, zfoc_illum=physical_dims[0]/2 - ls_pos,
+    m = biobeam.SimLSM_Cylindrical(dn = dn, signal = signal, zfoc_illum=(-1 if right_illum else 1) * physical_dims[0]/2 - ls_pos,
                        NA_illum= .1, NA_detect=.45,
                        n_volumes=2, lam_illum =lam/1000, lam_detect =lam/1000,
-                       size = physical_dims, n0 = 1.33)
+                       size = physical_dims, n0 = RI_DEFAULT)
     
     
     # make outdir if necessary
@@ -62,6 +68,8 @@ def sim_lightsheet(base_path, out_dir, phantom_dir='sim-phantom', in_pattern='si
 
     out = np.zeros((img.shape[0] - 16*2,) + img.shape[1:], dtype=img.dtype)
     for i in range(out.shape[0]):
+        if planes_to_simulate is not None and not i in planes_to_simulate:
+            continue
 
         cz = i - (img.shape[0] - 16*2) // 2
         cz = cz * m._bpm_detect.units[-1]
@@ -79,18 +87,18 @@ if __name__ == '__main__':
     base_dir = '/scratch/hoerl/sim_tissue/'
     
     params = [
-        (False, 250, 400),
-        (False, 400, 400),
-        (False, 550, 400),
-        (True, 250, 400),
-        (True, 400, 400),
-        (True, 550, 400),
-        (False, 250, 600),
-        (False, 400, 600),
-        (False, 550, 600),
-        (True, 250, 600),
-        (True, 400, 600),
-        (True, 550, 600)
+        (False, 125, 400),
+        (False, 200, 400),
+        (False, 325, 400),
+        (True, 125, 400),
+        (True, 200, 400),
+        (True, 325, 400),
+        (False, 125, 600),
+        (False, 200, 600),
+        (False, 325, 600),
+        (True, 125, 600),
+        (True, 200, 600),
+        (True, 325, 600)
     ]
     
     for (right_illum, ls_pos, lam) in params:
