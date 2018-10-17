f9ac18cdc2b7dce0c3514ef14e5d0356438b5418
David Hörl <hoerldavid@gmail.com>
2018-10-17 11:38:54 +0200

    working simulation definition, moved common methods to sim_util, commandline run via run_sim2

---
+++ run_sim2.py
@@ -0,0 +1,25 @@
+import argparse
+import gputools
+from sim_util import sim_from_definition
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Run biobeam simulation')
+    parser.add_argument('params_file', type=str,
+                        help='JSON file containing the parameters of the simulation')
+    parser.add_argument('--gpu_idx', dest='gpu_idx', default=0,
+                        help='Index of the GPU to use for simulation')
+
+    args = parser.parse_args()
+
+    # set and check which gpu is used
+    gputools.init_device(id_platform=0, id_device=args.gpu_idx)
+    gputools.get_device()
+
+    # run simulation
+    sim_from_definition(args.params_file)
+
+
+if __name__ == '__main__':
+    main()
--- sim_util.py
+++ sim_util.py
@@ -43,3 +43,171 @@ def random_points_in_volume_min_distance(low, high, min_dist, n_points):
             points.append(candidate)
 
     return np.array(points)
+
+from run_sim import dn_from_signal
+from calmutils.misc import split_str_digit
+from run_sim import simple_downsample
+from run_sim import RI_DEFAULT, RI_DELTA_RANGE
+from skimage.io import imread_collection
+import biobeam
+import os
+import re
+from skimage.io import imsave
+
+
+CONV_PADDING = 16
+
+
+def save_as_sequence(img, base_path, file_pattern='image{plane}.tif', pad_idx=True, axis=0):
+    
+    if not os.path.exists(base_path):
+        os.makedirs(base_path)
+    
+    for plane in range(img.shape[axis]):
+        idx = tuple([slice(img.shape[ax]) if ax!=axis else plane for ax in range(len(img.shape))])
+        img_i = img[idx].astype(np.float32)
+        plane_padded = plane
+        if pad_idx:
+            pref = '0' * (len(str(img.shape[axis]-1)) - len(str(plane)))
+            plane_padded = pref + str(plane_padded)
+        imsave(os.path.join(base_path, file_pattern.format(plane=plane_padded)), img_i)
+        
+        
+def load_tiff_sequence(raw_data_path, pattern=None):
+    # load data
+    files = os.listdir(raw_data_path)
+    files.sort(key=lambda f: split_str_digit(f))
+
+    # read raw phantom
+    ic = imread_collection([os.path.join(raw_data_path, f) for f in files if (re.match(pattern, f) if pattern else True)])
+    img = ic.concatenate()
+    return img
+
+
+def sim_lightsheet_img(img, desc, dn, right_illum,
+                   na_illum, na_detect,
+                   physical_dims=(400,400,50), ls_pos=200, lam=500,
+                   ri_medium=RI_DEFAULT, ri_range=RI_DELTA_RANGE):
+    
+    # zero-pad image for conv
+    img = np.pad(img, ((CONV_PADDING,CONV_PADDING),(0,0),(0,0)), "constant")
+    desc = np.pad(desc, ((CONV_PADDING,CONV_PADDING),(0,0),(0,0)), "constant")
+    dn = np.pad(dn, ((CONV_PADDING,CONV_PADDING),(0,0),(0,0)), "constant")
+    
+    # simulate right illumination with flip of x-axis
+    if right_illum:
+        img = np.flip(img, 2)
+        desc = np.flip(desc, 2)
+        dn = np.flip(dn, 2)
+    
+    # create a microscope simulator for signal
+    m = biobeam.SimLSM_Cylindrical(dn = dn, signal = img, zfoc_illum=(-1 if right_illum else 1) * physical_dims[0]/2 - ls_pos,
+                       NA_illum=na_illum, NA_detect=na_detect,
+                       n_volumes=4, lam_illum =lam/1000, lam_detect =lam/1000,
+                       size = physical_dims, n0 = ri_medium)
+    
+    # create a microscope simulator for descriptors
+    m_desc = biobeam.SimLSM_Cylindrical(dn = dn, signal = desc, zfoc_illum=(-1 if right_illum else 1) * physical_dims[0]/2 - ls_pos,
+                       NA_illum=na_illum, NA_detect=na_detect,
+                       n_volumes=4, lam_illum =lam/1000, lam_detect =lam/1000,
+                       size = physical_dims, n0 = ri_medium)
+    
+    
+    out = np.zeros((img.shape[0] - CONV_PADDING*2,) + img.shape[1:], dtype=img.dtype)
+    out_desc = np.zeros((img.shape[0] - CONV_PADDING*2,) + img.shape[1:], dtype=img.dtype)
+    for i in range(out.shape[0]):
+
+        cz = i - (img.shape[0] - CONV_PADDING*2) // 2
+        cz = cz * m._bpm_detect.units[-1]
+
+        image = m.simulate_image_z(cz=cz, zslice=CONV_PADDING, psf_grid_dim=(16,16), conv_sub_blocks=(4,4))
+        out[i] = image[CONV_PADDING] if not right_illum else np.flip(image[CONV_PADDING], 1)
+        
+        image = m_desc.simulate_image_z(cz=cz, zslice=CONV_PADDING, psf_grid_dim=(16,16), conv_sub_blocks=(4,4))
+        out_desc[i] = image[CONV_PADDING] if not right_illum else np.flip(image[CONV_PADDING], 1)
+
+    return out, out_desc
+    
+
+def sim_from_definition(def_path):
+    
+    # load settings
+    with open(def_path, 'r') as fd:
+        params = json.load(fd)
+        
+    save_fstring = params['save_fstring']
+    seed = params['seed']
+    raw_data_path = params['raw_data_path']
+    raw_data_dims = params['raw_data_dims']
+    downsampling = params['downsampling']
+    phys_dims = params['phys_dims']
+    na_illum = params['na_illum']
+    na_detect = params['na_detect']
+    ri_medium =  params['ri_medium']
+    ri_delta_range = params['ri_delta_range']
+    two_sided_illum = params['two_sided_illum']
+    lambdas = params['lambdas']
+    fov_size = params['fov_size']
+    point_fov_size = params['point_fov_size']
+    n_points_per_fov = params['n_points_per_fov']
+    points_min_distance = params['points_min_distance']
+    min_off = params['min_off']
+    max_off = params['max_off']
+    x_locs = params['x_locs']
+    y_locs = params['y_locs']
+    z_locs = params['z_locs']
+
+    img = load_tiff_sequence(raw_data_path)
+    
+    # LOG: loaded raw data
+
+    # downsample img
+    if downsampling > 1:
+        img = simple_downsample(img, downsampling)
+        # LOG: downsampled
+
+    dn = dn_from_signal(img, ri_medium, ri_delta_range)
+
+    # make descriptor img
+    desc_img = np.zeros_like(img)
+    for field in params['fields'].values():
+        for point in field['points']:
+            point = list(np.array(point) // downsampling)
+            desc_img[tuple(point)] = 1
+    
+    # TODO: blur slightly?
+
+    for lam, right_illum in zip(lambdas, [False] if not two_sided_illum else [True, False]):
+
+        for xi in range(len(x_locs)):
+
+            physical_dims_ = tuple(list(np.array(phys_dims)//downsampling))
+            ls_pos_ = np.interp(x_locs[xi]//downsampling, (0, raw_data_dims[2]//downsampling), (0, phys_dims[2]//downsampling))
+
+            # simulate signal and descriptors
+            out_signal, out_desc = sim_lightsheet_img(img, desc_img, dn, right_illum, na_illum, na_detect, physical_dims_, ls_pos_,
+                               lam, ri_medium, ri_delta_range)
+
+            yidx, zidx = np.meshgrid(range(len(y_locs)), range(len(z_locs)))    
+            for yi, zi in zip(yidx.flat, zidx.flat):
+
+                field_info = fields[','.join(map(str, (xi, yi, zi)))]
+                off = field_info['off']
+                loc = [z_locs[zi], y_locs[yi], x_locs[xi]]
+                min_ = np.array(loc) + np.array(off) - np.ceil(np.array(fov_size) / 2)
+                max_ = np.array(loc) + np.array(off) + np.floor(np.array(fov_size) / 2)
+
+                # out dir
+                out_dir = save_fstring.format(x=xi, y=yi, z=zi, lam=lam, illum=1 if not right_illum else 2)
+
+                # cut signal and descriptors
+                # NB: downsampling
+                min_ = min_//downsampling
+                max_ = max_//downsampling
+
+                idxs = tuple([slice(int(mi), int(ma)) for mi, ma in zip(min_, max_)])
+
+                # save
+                save_as_sequence(out_signal[idxs], out_dir, file_pattern='bbeam_sim_c1_z{plane}.tif')
+                save_as_sequence(out_desc[idxs], out_dir, file_pattern='bbeam_sim_c2_z{plane}.tif')
+
